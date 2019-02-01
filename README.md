e495b13dee379b614cb37c0a6ee784cde822c571
Mohsen Mahmoodi <mo.ttds.g@gmail.com>
2019-02-01 17:36:09 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,107 @@
+# Django 2.0+ project template
+
+This is a personalized fork of [Django 2.0+ project template](https://jpadilla.github.io/django-project-template/) 
+with all the basic requirements as used in my projects. The original template met my basic needs but I had to do a 
+lot of modifications before actually starting development. Because all my changes where personal preferences I 
+preferred to create a customized version of the project. Just like the original project, this template make the least 
+mount of assumptions possible while still trying provide a useful setup. Also the base project template has 
+configurations to support [heroku](https://heroku.com/) deployment and since I use [heroku](https://heroku.com/) most of
+ the time I will support it in this template too. 
+
+## Features
+
+- [Django](https://www.djangoproject.com/) 2.0+
+- Based on [Pipenv](https://github.com/kennethreitz/pipenv) - the officially recommended Python packaging tool from Python.org.
+- Development, Staging and Production settings with [django-configurations](https://django-configurations.readthedocs.org).
+- Get value insight and debug information while on Development with [django-debug-toolbar](https://django-debug-toolbar.readthedocs.org).
+- Collection of custom extensions with [django-extensions](http://django-extensions.readthedocs.org).
+- HTTPS and other security related settings on Staging and Production.
+- Procfile for running gunicorn with New Relic's Python agent.
+- PostgreSQL database support with psycopg2.
+- Externalized logging configuration based on [Good logging practice in Python](https://fangpenlin.com/posts/2012/08/26/good-logging-practice-in-python/)
+
+## How to install
+
+```bash
+pip3 install pipenv
+mkdir project_name
+cd project_name
+pipenv --python <version>
+pipenv install django
+pipenv run django-admin.py startproject \
+  --template=https://github.com/mohsen-mahmoodi/django2-project-template/archive/master.zip \
+  --name=Procfile \
+  --extension=py,md,env,yaml \
+  project_name .
+pipenv install -r requirements/common.txt
+pipenv install -r requirements/dev.txt --dev
+rm -rf requirements
+cp example.env .env
+cp example.logging.yaml logging.yaml
+```
+
+## Environment variables
+
+These are common between environments. The `ENVIRONMENT` variable loads the correct settings, possible values are: `DEVELOPMENT`, `STAGING`, `PRODUCTION`.
+
+```dotenv
+ENVIRONMENT=DEVELOPMENT
+DJANGO_SECRET_KEY=dont-tell-eve
+DJANGO_DEBUG=yes
+...
+```
+
+These settings(and their default values) are only used on staging and production environments.
+
+```dotenv
+DJANGO_SESSION_COOKIE_SECURE=yes
+DJANGO_SECURE_BROWSER_XSS_FILTER=yes
+DJANGO_SECURE_CONTENT_TYPE_NOSNIFF=yes
+DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS=yes
+DJANGO_SECURE_HSTS_SECONDS=31536000
+DJANGO_SECURE_REDIRECT_EXEMPT=
+DJANGO_SECURE_SSL_HOST=
+DJANGO_SECURE_SSL_REDIRECT=yes
+DJANGO_SECURE_PROXY_SSL_HEADER=HTTP_X_FORWARDED_PROTO,https
+```
+
+## Deployment
+
+It is possible to deploy to Heroku or to your own server.
+
+### Heroku
+
+```bash
+heroku create
+heroku addons:add heroku-postgresql:hobby-dev
+heroku pg:promote DATABASE_URL
+heroku config:set ENVIRONMENT=PRODUCTION
+heroku config:set DJANGO_ALLOWED_HOSTS='[app-name].herokuapp.com'
+heroku config:set DJANGO_SECRET_KEY=`./manage.py generate_secret_key`
+git push heroku master
+heroku run python manage.py createsuperuser
+```
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2019 Mohsen Mahmoodi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ project_name/settings.py
@@ -0,0 +1,206 @@
+"""
+Django settings for {{ project_name }} project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/{{ docs_version }}/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/
+"""
+import os
+import yaml
+
+from configurations import Configuration, values
+
+
+def load_logging_config(path):
+    """
+    Loads the logging configuration from a yaml file and returns the configuration as a dictionary
+    :return :logging configuration dictionary
+    """
+
+    if os.path.exists(path):
+        try:
+            with open(path, 'rt') as f:
+                return yaml.safe_load(f.read())
+        except (IOError, ValueError) as e:
+            print("Error loading logging configuration.")
+            print(e)
+    return {}
+
+
+class Common(Configuration):
+    # This is the project's root directory where the manage.py file exists.
+    # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+    BASE_DIR = os.path.normpath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+    # This is the directory under which all the assets will be stored
+    ASSETS_DIR = os.path.join(BASE_DIR, 'assets')
+
+    # Directory for uploaded media files and collected static files
+    CONTENTS_DIR = os.path.join(BASE_DIR, 'contents')
+
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = values.SecretValue()
+
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = values.BooleanValue(False)
+
+    ALLOWED_HOSTS = values.ListValue([])
+
+    # Application definition
+    INSTALLED_APPS = [
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'whitenoise.runserver_nostatic',
+        'django.contrib.staticfiles',
+
+        'django_extensions',
+
+        '{{ project_name }}.membership.apps.MembershipConfig',
+    ]
+
+    MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',
+        'whitenoise.middleware.WhiteNoiseMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
+
+    ROOT_URLCONF = '{{ project_name }}.urls'
+
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [os.path.join(ASSETS_DIR, 'templates')],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
+
+    WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
+
+    APPEND_SLASH = values.BooleanValue(False)
+
+    # Database
+    # https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
+    DATABASES = values.DatabaseURLValue(
+        'sqlite:///{}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
+
+    # Password validation
+    # https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#auth-password-validators
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
+
+    # Internationalization
+    # https://docs.djangoproject.com/en/{{ docs_version }}/topics/i18n/
+    LANGUAGE_CODE = values.Value('en-us')
+
+    TIME_ZONE = 'UTC'
+
+    USE_I18N = True
+
+    USE_L10N = True
+
+    USE_TZ = True
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/{{ docs_version }}/howto/static-files/
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+    MEDIA_URL = values.Value('/media/')
+
+    STATIC_URL = values.Value('/statics/')
+
+    MEDIA_ROOT = os.path.join(CONTENTS_DIR, 'media')
+
+    STATIC_ROOT = os.path.join(CONTENTS_DIR, 'statics')
+
+    STATICFILES_DIRS = (
+        os.path.join(ASSETS_DIR, 'static'),
+    )
+
+    LOCALE_PATHS = (
+        os.path.join(ASSETS_DIR, 'locale'),
+    )
+
+    FIXTURE_DIRS = (
+        os.path.join(ASSETS_DIR, 'fixtures'),
+    )
+
+    AUTH_USER_MODEL = 'membership.User'
+
+    LOGGING = load_logging_config('logging.yaml')
+
+
+class Development(Common):
+    """
+    The in-development settings and the default configuration.
+    """
+    DEBUG = True
+
+    ALLOWED_HOSTS = ['*']
+
+    INTERNAL_IPS = [
+        '127.0.0.1'
+    ]
+
+    INSTALLED_APPS = Common.INSTALLED_APPS + [
+        'debug_toolbar'
+    ]
+
+    MIDDLEWARE = Common.MIDDLEWARE + [
+        'debug_toolbar.middleware.DebugToolbarMiddleware'
+    ]
+
+
+class Staging(Common):
+    """
+    The in-staging settings.
+    """
+    # Security
+    SESSION_COOKIE_SECURE = values.BooleanValue(True)
+    SECURE_BROWSER_XSS_FILTER = values.BooleanValue(True)
+    SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
+    SECURE_HSTS_SECONDS = values.IntegerValue(31536000)
+    SECURE_REDIRECT_EXEMPT = values.ListValue([])
+    SECURE_SSL_HOST = values.Value(None)
+    SECURE_SSL_REDIRECT = values.BooleanValue(True)
+    SECURE_PROXY_SSL_HEADER = values.TupleValue(
+        ('HTTP_X_FORWARDED_PROTO', 'https')
+    )
+
+
+class Production(Staging):
+    """
+    The in-production settings.
+    """
+    pass
